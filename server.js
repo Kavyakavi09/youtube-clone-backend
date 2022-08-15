e14d28e14492ae7f9936e6cdf6f5ac899af3e649
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-15 11:46:27 +0530

    routes added

--- server.js
+++ server.js
@@ -2,19 +2,39 @@ import express from 'express';
 import cors from 'cors';
 import connect from './db/connectDb.js';
 import dotenv from 'dotenv';
-// import productRoutes from './routes/product.js';
+import userRoutes from './routes/users.js';
+import videoRoutes from './routes/videos.js';
+import commentRoutes from './routes/comments.js';
+import authRoutes from './routes/auth.js';
+import cookieParser from 'cookie-parser';
 
 // web server
 const app = express();
-app.use(express.json());
 
 app.use(cors());
 
 // dotenv environment setup
 dotenv.config();
 
-// // To connected with routes
-// app.use('/api/product', productRoutes);
+// middlewares
+app.use(cookieParser());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/videos', videoRoutes);
+app.use('/api/comments', commentRoutes);
+
+//error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || 'Something went wrong!';
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+    stack: err.stack,
+  });
+});
 
 let port = process.env.PORT || 4016;
 
