745057133e8e697baefe077d3277d1466c97dd89
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-25 12:02:16 +0530

    going

--- verifyToken.js
+++ verifyToken.js
@@ -3,12 +3,13 @@ import { createError } from './error.js';
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token || token == undefined)
-    return next(createError(401, 'You are not authenticated!'));
+  console.log(token);
+  if (!token) return next(createError(401, 'You are not authenticated!'));
 
   jwt.verify(token, process.env.JWT, (err, user) => {
     if (err) return next(createError(403, 'Token is not valid!'));
     req.user = user;
+
     next();
   });
 };
