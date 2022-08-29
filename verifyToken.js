b04558ecd0f65c5f9ad587db55d9ea72feee6c65
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-08-29 09:51:03 +0530

    cookie set

--- verifyToken.js
+++ verifyToken.js
@@ -1,13 +1,15 @@
-import jwt from "jsonwebtoken";
-import { createError } from "./error.js";
+import jwt from 'jsonwebtoken';
+import { createError } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization || req.headers.Authorization;
+  // const token = req.headers.authorization || req.headers.Authorization;
+  const cookies = req.headers.cookie;
+  token = cookies.split('=')[1];
 
-  if (!token) return next(createError(401, "You are not authenticated!"));
+  if (!token) return next(createError(401, 'You are not authenticated!'));
 
   jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Token is not valid!"));
+    if (err) return next(createError(403, 'Token is not valid!'));
     req.user = user;
 
     next();
